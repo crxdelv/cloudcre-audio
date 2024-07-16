7d55425aedf7f9aa5c27e65d967a70428b3d6b1f
si delv <151720755+creuserr@users.noreply.github.com>
2024-07-16 23:52:16 +0800

    update main.py

--- api/main.py
+++ api/main.py
@@ -5,7 +5,6 @@ import traceback
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
-    dispose = 'dispose' in self.path
     try:
       yt = YouTube(self.path)
       streams = yt.streams.filter(only_audio=True)
@@ -14,7 +13,8 @@ class handler(BaseHTTPRequestHandler):
       self.send_response(200)
       self.send_header('Content-Type', 'audio/mp4')
       self.send_header('Access-Control-Allow-Origin', '*')
-      self.send_header('Content-Disposition', 'attachment')
+      if 'dispose' in self.path:
+       self.send_header('Content-Disposition', 'attachment')
       with open('/tmp/output.mp3', 'rb') as f:
         self.wfile.write(f.read())
     except:
