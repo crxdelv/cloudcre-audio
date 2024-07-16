dd073691fd12b47fa005ff5eac69e3a3837a9ca5
si delv <151720755+creuserr@users.noreply.github.com>
2024-07-17 00:24:43 +0800

    update main.py

--- api/main.py
+++ api/main.py
@@ -1,20 +1,36 @@
+from pytubefix import YouTube
 from http.server import BaseHTTPRequestHandler
-import traceback, requests
+import traceback
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
+    if len(self.path) < 2:
+      return self.end('400 Bad Request: Incomplete Parameter', 400)
+    path = None
     try:
-      req = requests.get('https:/' + self.path, stream=True)
-      req.raise_for_status()
-      self.send_header('Content-Type', 'audio/mp4')
-      self.send_header('Access-Control-Allow-Origin', '*')
-      self.send_header('Content-Disposition', 'attachment')
-      self.send_response(200)
-      for chunk in req.iter_content(chunk_size=8192):
-        self.wfile.write(chunk)
+      path = self.path.split("/")[1]
     except:
-      self.send_header('Content-Type', 'text/plain')
+      return self.end('400 Bad Request: Invalid Parameter', 400)
+    try:
+      video = YouTube('https://youtu.be/' + path)
+      stream = video.streams.filter(only_audio=True).first()
+      stream.download(filename='output.mp3', output_path='/tmp/')
+      self.send_response(200)
+      self.send_header('Content-type', 'audio/mp3')
       self.send_header('Access-Control-Allow-Origin', '*')
-      self.send_response(500)
       self.end_headers()
-      self.wfile.write(traceback.format_exc().encode('utf-8'))
+      with open('/tmp/output.mp3', 'rb') as f:
+        self.wfile.write(f.read())
+        return f.close()
+    except Exception as e:
+      if('regex_search' in str(e)):
+        return self.end('400 Bad Request: Invalid Parameter', 400)
+      if('unavailable' in str(e)):
+        return self.end('404 Not Found: Unavailable', 404)
+      traceback.print_exc()
+  def end(self, text, status):
+    self.send_response(status)
+    self.send_header('Content-type', 'text/plain')
+    self.send_header('Access-Control-Allow-Origin', '*')
+    self.end_headers()
+    self.wfile.write(text.encode('utf-8'))
