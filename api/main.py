99d9f3dc5868f406662f16976e45bb3eaf50fe47
si delv <151720755+creuserr@users.noreply.github.com>
2024-07-17 00:17:08 +0800

    update main.py

--- api/main.py
+++ api/main.py
@@ -6,15 +6,15 @@ class handler(BaseHTTPRequestHandler):
     try:
       req = requests.get('https:/' + self.path, stream=True)
       req.raise_for_status()
-      self.send_response(200)
       self.send_header('Content-Type', 'audio/mp4')
       self.send_header('Access-Control-Allow-Origin', '*')
       self.send_header('Content-Disposition', 'attachment')
+      self.send_response(200)
       for chunk in req.iter_content(chunk_size=8192):
         self.wfile.write(chunk)
     except:
-      self.send_response(500)
       self.send_header('Content-Type', 'text/plain')
       self.send_header('Access-Control-Allow-Origin', '*')
+      self.send_response(500)
       self.end_headers()
       self.wfile.write(traceback.format_exc.encode('utf-8'))
