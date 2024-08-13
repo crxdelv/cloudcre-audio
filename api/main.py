da308f8ab41184c14c5f4d4af8ebfd3759c8a072
si delv <151720755+crxdelv@users.noreply.github.com>
2024-08-13 18:22:35 +0800

    update main.py

--- api/main.py
+++ api/main.py
@@ -1,6 +1,14 @@
 from pytubefix import YouTube
+from pytube. innertube import _default_clients
 from http.server import BaseHTTPRequestHandler
 import traceback
+
+_default_clients[ "ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
+_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
